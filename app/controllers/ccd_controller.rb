43152acd8310dfcc8a21fff9d80657cf14f8c329
Todd Schwartzfarb <todd@wirestreak.com>
2016-08-13 19:32:29 -0400

    initial commit'

---
+++ app/controllers/ccd_controller.rb
@@ -0,0 +1,17 @@
+class CcdController < ApplicationController
+  def show
+    require 'open-uri'
+
+    #  url = "http://services.development.carecloud.local/ccd/2460-9271986201"  # original from Grady
+     url = "http://services.development.carecloud.local/ccd/0029-1418297147"  # pattis patient
+
+
+    @contents =  open(url).read
+
+    render json: @contents
+  end
+
+  def home
+    @welcome = "Welcome to WynCare!!!"
+  end
+end
