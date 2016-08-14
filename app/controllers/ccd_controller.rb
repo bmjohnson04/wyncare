430b6f731e952326f40befd5e744ba76d3fbc83a
Brandon <bmjohnson04@gmail.com>
2016-08-13 20:57:35 -0400

    changed patient ID

--- app/controllers/ccd_controller.rb
+++ app/controllers/ccd_controller.rb
@@ -2,7 +2,7 @@ class CcdController < ApplicationController
   def show
     require 'open-uri'
 
-     url = "https://services.development.carecloud.com/ccd/2460-9271986201"  # original from Grady
+     url = "https://services.development.carecloud.com/ccd/0029-1418297147"  # original from Grady
     #  url = "http://services.development.carecloud.local/ccd/0029-1418297147"  # pattis patient
 
 
