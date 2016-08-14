93571c0c06810ca4ec76b1bd99ea039e7969d996
Brandon <bmjohnson04@gmail.com>
2016-08-14 03:31:42 -0400

    changed spacing

--- app/controllers/fbm_controller.rb
+++ app/controllers/fbm_controller.rb
@@ -1,32 +1,7 @@
 class FbmController < ApplicationController
-  def fbm_msg
-
-    respond_to do |format|
-        format.json{ render json: [{"text": "Hellooo, Nurse!"}] }
-      end
+  def get_stuff
+   respond_to do |format|
+    format.json{ render(json: [{"text": "Hellooo, Nurse!"}], status: :ok) }
+   end
   end
-  #   require 'open-uri'
-  #   require 'json'
-  #
-  #   if Rails.env.development?
-  #
-  #     url = "http://linktochatfuel.com"
-  #
-  #     chat_response =  open(url).read
-  #
-  #
-  #     chat_response = JSON.parse(chat)["text"]
-  #
-  #
-  #     response_array = []
-  #
-  #     chat_response.each do |msg|
-  #       response_array.push chat["text"]
-  #     end
-  #
-  #     @api_response = response_array.to_json
-  #     render json: @api_response
-  #
-  #   end
-  # end
 end
