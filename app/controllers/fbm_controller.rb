b1a0d26ec66c3d578f68d381c5b586634036aa38
Chris Scott <cyberstrike@gmail.com>
2016-08-14 03:57:08 -0400

    fixed endpoint

--- app/controllers/fbm_controller.rb
+++ app/controllers/fbm_controller.rb
@@ -1,5 +1,7 @@
 class FbmController < ApplicationController
   def get_stuff
-    render(json: [{"text": "Hellooo, Nurse!"}], status: :ok)
-  end
+    respond_to do | format |
+      format.json{ render json: '[{"text": "Hellooo, Nurse!"}]', status: :ok }
+    end
+  end
 end
