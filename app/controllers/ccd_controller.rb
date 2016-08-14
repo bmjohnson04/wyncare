9314750038c9ea2ab709dd951679d388a7411b72
Todd Schwartzfarb <todd@wirestreak.com>
2016-08-13 20:27:18 -0400

    change external URL for testing

--- app/controllers/ccd_controller.rb
+++ app/controllers/ccd_controller.rb
@@ -3,8 +3,9 @@ class CcdController < ApplicationController
     require 'open-uri'
 
     #  url = "http://services.development.carecloud.local/ccd/2460-9271986201"  # original from Grady
-     url = "http://services.development.carecloud.local/ccd/0029-1418297147"  # pattis patient
+    #  url = "http://services.development.carecloud.local/ccd/0029-1418297147"  # pattis patient
 
+     url = 'http://ruby.bastardsbook.com/files/fundamentals/hamlet.txt'
 
     @contents =  open(url).read
 
